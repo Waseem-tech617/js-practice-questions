87aebb434f4b1ff754ab155f50b4e75424de22cd
Malik Waseem <malikwaseem123456564@gmail.com>
2025-08-12 03:25:12 -0700

    add questions

--- revision.js
+++ revision.js
@@ -35,3 +35,40 @@ for(let i=0;i<=words.length-1;i++){
     }
 }
 console.log(count);
+
+
+// 4️⃣ Password Strength Checker.
+
+let uper=false;
+let lower=false;
+let digit=false;ssss
+let special=false;
+
+ let password=prompt("Enter a pasword: folow these at least 1 uppercase, 1 lowercase, 1 digit, aur 1 special character ");
+ for(let i=0;i<=password.length-1;i++){
+    if(password[i]===password[i].toLowerCase() && isNaN(password[i] )){
+        lower=true;}
+        else if(password[i]===password[i].toUpperCase() && isNaN(password[i])){
+            uper=true;
+        }else if(!isNaN(password[i])){
+            digit=true;
+        }
+        else if("!@#$%^&*".includes(password[i])){
+            special=true;
+        }
+    }
+    
+    
+    if(uper && lower&& digit&& special){
+        console.log("strong password");
+    }else{
+        console.log("weakpassword");
+    }
+    
+    
+    
+    
+ 
+    
+    
+   
