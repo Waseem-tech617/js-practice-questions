a7e3d5ede89eebb174a9a6eb5ef2bc515d71a0fb
Malik Waseem <malikwaseem123456564@gmail.com>
2025-08-20 06:17:03 +0500

    add questions

--- functions.js
+++ functions.js
@@ -4,3 +4,17 @@ function bmi(weight,height){
     return weight/(height*height);
 }
 console.log(bmi(35,1.5).toFixed(1));
+
+
+// Q2.Discount Calculator.
+
+function Discountcalculator(discount){
+    return function(value){
+        return value-value*(discount/100);
+    }
+
+}
+let ten=Discountcalculator(10);
+console.log(ten(1200));
+console.log(ten(200));
+
