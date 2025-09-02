0aee21714faced540d93eef8946277452e712c9a
Malik Waseem <malikwaseem123456564@gmail.com>
2025-09-02 10:01:54 +0500

    add

--- arrays.js
+++ arrays.js
@@ -448,3 +448,19 @@ if(narr===arr) {
 }else{
     console.log("not");
 }
+
+
+// Q41.Sare zeros ko end me le jao (without new array).
+ let arr = [1, 0, 2, 0, 3, 0, 4];
+ let count=0;
+ for(let i=0;i<arr.length;i++){
+    if(arr[i]!==0){
+        arr[count]=arr[i];
+        count++;
+    }
+ }
+ while(count<arr.length){
+        arr[count]=0;
+        count++;
+    }
+ console.log(arr);
