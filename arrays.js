bf76bcf016847b55647f9655bb32872d8e765a5b
Malik Waseem <malikwaseem123456564@gmail.com>
2025-09-05 07:49:51 +0500

    add

--- arrays.js
+++ arrays.js
@@ -565,9 +565,12 @@ for(let i of arr){
 
 // Q50.Array ke har possible pair print karo.
 let arr = [1, 2, 3, 4, 5];
-
 for (let i = 0; i < arr.length; i++) {
+      let sum=0;
     for (let j = i + 1; j < arr.length; j++) {
-        console.log([arr[i], arr[j]]);
+      console.log( [arr[i]+arr[j]]); 
     }
 }
+
+
+
