5e1320e75b755d33577e7ebe7bd20d5eeb0c8780
Malik Waseem <malikwaseem123456564@gmail.com>
2025-09-05 07:57:08 +0500

    add

--- arrays.js
+++ arrays.js
@@ -573,11 +573,26 @@ for (let i = 0; i < arr.length; i++) {
 }
 
 
-// Q50.Array ke har possible pair ka product  print karo.
+// Q51.Array ke har possible pair ka product  print karo.
 let arr = [1, 2, 3, 4, 5];
 for (let i = 0; i < arr.length; i++) {
-      let sum=0;
     for (let j = i + 1; j < arr.length; j++) {
       console.log( [arr[i]*arr[j]]); 
+    }
+}
+
+
+// Q52.Array ke har possible pair ka product  print karo.
+let arr = [1, 2, 3, 4, 5];
+for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      sum=arr[i]+arr[j];
+      if(sum%2==0){
+        console.log(`${sum} is even`);
+      }
+      else{
+         console.log(`${sum} is odd`);
+      }
+
     }
 }
