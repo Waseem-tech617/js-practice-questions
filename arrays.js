1828970832eea3e082b80dd71aeaa6504f23d8f8
Malik Waseem <malikwaseem123456564@gmail.com>
2025-09-09 07:11:50 +0500

    add

--- arrays.js
+++ arrays.js
@@ -743,3 +743,15 @@ let discount=prices.map((val)=>{
 });
 console.log(discount);
 
+
+// Q64. Ek 2D array banao (matrix form me) aur usko print karo row by row.
+
+let arr = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+];
+for(let i=0;i<arr.length;i++){
+        console.log(arr[i]);
+}
+
