fc874fde8be53146444a23259265f54d706fd691
Malik Waseem <malikwaseem123456564@gmail.com>
2025-09-12 07:46:55 +0500

    add

--- objects.js
+++ objects.js
@@ -70,7 +70,7 @@ console.log(student.subjects[0]);
 student.subjects.push("History");
 
 
-// Q3. Array of Objects inside Object
+// Q7. Array of Objects inside Object
 // Ek company object banao jisme:
 // name: "TechCorp"
 // employees: [
@@ -94,3 +94,30 @@ company.employees[0].role="UI/UX Designer";
 console.log(company.employees[0].name);
 console.log(company.employees[0].role);
 console.log(company.employees);
+
+
+
+// Q8.Ek product object banao jisme:
+// id: 101
+// title: "Laptop"
+// price: 80000
+// variants: [
+//    { color: "Black", stock: 12 },
+//    { color: "Silver", stock: 5 }
+// ]
+// Silver variant ka stock print karo
+// Black variant ka stock update karke 10 kar do
+// Ek naya variant {color: "Blue", stock: 7} add karo
+let product={
+       id: 101,
+       title: "Laptop",
+       price: 80000,
+       variants: [
+                { color: "Black", stock: 12 },
+                { color: "Silver", stock: 5 }
+]
+}
+product.variants[0].stock=10;
+product.variants.push({color:"Black", stock:7});
+console.log(product.variants[0].stock);
+console.log(product.variants);
