5b399c1a68629f175a54afb4233554b6bc4b88b1
Malik Waseem <malikwaseem123456564@gmail.com>
2026-01-30 12:53:31 +0500

    add

--- Web-Dev-Practice/Practice-Basics.js
+++ Web-Dev-Practice/Practice-Basics.js
@@ -1,9 +1,9 @@
-// // Practice uniray operator.
+// // // Practice uniray operator.
 
-let a=11;
-let b=22;
-let c=a + b + a++ + b++ + ++a + ++b;11+22+11+22+13+24
-console.log(a);
-console.log(b);
-console.log(c);
+// let a=11;
+// let b=22;
+// let c=a + b + a++ + b++ + ++a + ++b;11+22+11+22+13+24
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
